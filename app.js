74cfba92db8018d8227e89c6f0187c8b468ea68b
Nicolas dos Santos <nicolas182@icloud.com>
2019-02-22 14:05:34 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get("/", (req, res)=>{
+    res.render('index');
+});
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.listen(7000, ()=>{
+    console.log("Listening on port 7000...");
+});
+
+
