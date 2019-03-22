fadc893a21d8f20985f207a86f62fc0e32d05ac6
Nicolas dos Santos <nicolas182@icloud.com>
2019-03-22 08:04:09 -0400

    Password reset is tested and ready to be merged

--- routes/members.js
+++ routes/members.js
@@ -445,7 +445,6 @@ router.get('/logout', (req, res) => {
     req.logOut();
     req.flash('cardSuccess', 'You are logged out');
     res.redirect('/');
-});
 
 //Account settings route
 router.get('/account', (req, res) => {
