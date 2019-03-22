ccafaf7b2045f2e69ea8e685f5c9f6973bd158aa
Nicolas dos Santos <nicolas182@icloud.com>
2019-03-22 14:31:14 -0400

    Changing Branche

--- routes/projects.js
+++ routes/projects.js
@@ -3,23 +3,22 @@ const router = express.Router();
 const functions = require('../app');
 const flash = require('connect-flash');
 
-
+//Bring in models
 let Member = require('../models/member');
 let Project = require('../models/project');
 
 router.get('/home', (req,res)=>{
     
-    Project.find({}, (err, member) => {
+    Project.find({members:{$in:req.user._id}}, (err, project) => {
         if (err) {
             console.log(err);
         } else {
             res.render('home', {
-                title: "Member",
-                member: member
+                title: "Projects",
+                project: project
             });
         }
     })
-
 });
 
 router.get('/new_project', (req, res)=>{
@@ -35,6 +34,21 @@ router.post('/new_project', (req, res)=>{
     let member = req.user._id;
 
     //Field verification here:
+    req.checkBody('name', 'Name field is required').notEmpty();
+    req.checkBody('ishackathon', 'Hackathon field is required').notEmpty();
+    req.checkBody('description', 'Description fiels is required').notEmpty();
+    req.checkBody('git', 'Git URL is required').notEmpty();
+
+    let errors = req.validationErrors();
+
+    if (errors) {
+        //Display errors if any
+        res.render('new_project', {
+            errors: errors
+        });
+
+     } else {
+
     let newProject = new Project({
         name: name,
         ishackathon: isHackathon,
@@ -46,7 +60,7 @@ router.post('/new_project', (req, res)=>{
 
     });
 
-    newProject.members.push(req.user._id)
+    newProject.members.push(req.user._id);
 
     newProject.save((err)=>{
         if(err){
@@ -57,11 +71,7 @@ router.post('/new_project', (req, res)=>{
             res.redirect('/');
         }
     })
-
-
-
-
-
+}
 
 });
    
