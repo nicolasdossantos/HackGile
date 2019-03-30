25539ceb24073e0f73cce092785c377afa529780
Thomas Chin <tug43448@temple.edu>
2019-03-30 11:22:28 -0400

    untested stories api

---
+++ routes/api/stories.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('../../config/keys.js');
+const app = require('../../app.js');
+let Story = require('../../models/story');
+
+const router = express.Router();
+
+router.get('/:id', async (req, res) => {
+    const stories = await loadStoriesCollection();
+    res.send(await stories.find({member: mongoose.Types.ObjectId(req.params.id)}).toArray());
+});
+
+router.post('/', async (req, res) => {
+    const stories = await loadStoriesCollection();
+    let newStory = new Story({
+        project: req.body.project,
+        sprint: req.body.sprint,
+        status: req.body.status,
+        member: req.body.member,
+        title: req.body.title,
+        description: req.body.description,
+        estimatedTime: req.body.estimatedTime
+    });
+    await newStory.save(err => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    res.status(201).send();
+});
+
+router.put('/:id', async (req, res) => {
+    const stories = await loadStoriesCollection();
+    stories.findOneAndUpdate(
+        {_id: mongoose.Types.ObjectId(req.params.id)},
+        {
+            project: req.body.project,
+            sprint: req.body.sprint,
+            status: req.body.status,
+            member: req.body.member,
+            title: req.body.title,
+            description: req.body.description,
+            estimatedTime: req.body.estimatedTime
+        });
+    res.status(200).send();
+})
+
+router.delete('/:id', async (req, res) => {
+    const posts = await loadStoriesCollection();
+    await posts.deleteOne({_id: mongoose.Types.ObjectId(req.params.id)});
+    res.status(200).send();
+})
+
+async function loadStoriesCollection(){
+    return app.collection('stories');
+}
+
+module.exports = router;
