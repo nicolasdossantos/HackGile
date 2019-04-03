781c81f2774ee3c791569f2e215c433b71293cba
Thomas Chin <tug43448@temple.edu>
2019-04-03 02:03:05 -0400

    installed vuetify and vee-validate for future usage

--- vue-client/src/main.js
+++ vue-client/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VeeValidate from 'vee-validate';
+Vue.use(VeeValidate);
 
 Vue.config.productionTip = false
 
