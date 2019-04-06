61641fec049c485a14db40628afc921f264621c1
Nicolas dos Santos <nicolas182@icloud.com>
2019-04-06 14:01:57 -0400

    Draggable cards are there

--- vue-client/src/main.js
+++ vue-client/src/main.js
@@ -3,6 +3,15 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './utils/polyfills'
+
+import './assets/layout.css'
+import './assets/header.css'
+import './assets/nav.css'
+import './assets/demos.css'
+import './assets/form.css'
+
+import Demo from './index.vue'
 import VeeValidate from 'vee-validate';
 Vue.use(VeeValidate);
 
@@ -11,5 +20,7 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  components: { Demo },
+  template: '<Demo/>',
   render: h => h(App)
 }).$mount('#app')
--- vue-client/src/views/Home.vue
+++ vue-client/src/views/Home.vue
@@ -6,17 +6,20 @@
     <Navbar></Navbar>
     <SprintCard id="5ca7ab051c9d44000043c95f"></SprintCard>
     <SprintCard id="5ca7afcf1c9d4400008ef9d2"></SprintCard>
+    <Card></Card>
   </v-layout>
 </template>
 
 <script>
   import SprintCard from '../components/SprintCard'
   import Navbar from '../components/Navbar'
+  import Card from '../components/Card'
 
   export default {
     components: {
       SprintCard,
-      Navbar
+      Navbar,
+      Card
     }
   }
 </script>
