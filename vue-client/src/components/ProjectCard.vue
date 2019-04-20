7af9b5095ab26cd2df1b260b5f3682d21475c7cc
Thomas Chin <tug43448@temple.edu>
2019-04-19 21:37:40 -0400

    git image now has link and My Stories only shows non-done stories

--- vue-client/src/components/ProjectCard.vue
+++ vue-client/src/components/ProjectCard.vue
@@ -107,10 +107,17 @@
     <v-footer app inset height="auto">
       <v-card class="flex green lighten-3" flat tile>
          <v-card-title style="padding-bottom:0px; padding-top:10px">
+           <v-layout row>
+             <v-flex md10>
           <strong class="title">Members</strong>
           <AddMemberForm 
           v-bind:id="this.$props.id"
           @member-form-complete="$emit('member-form-complete')" />
+             </v-flex>
+             <v-flex md2>
+               <h3 class="text-xs-right">Access Your Project</h3>
+             </v-flex>
+           </v-layout>
         </v-card-title>
         <v-card-actions>
           <v-layout row>
@@ -139,8 +146,9 @@
             <v-flex v-if="git" xs1>
               <!-- Shows git link here -->
               <v-list-tile>
-                <v-img :src="gitImage" alt="TEXT" contain height="" width=""/>
-               
+                <a :href="git">
+                  <img :src="gitImage" alt="Git Link" style="height:90%; width:90%"/>
+                </a>
               </v-list-tile>
             </v-flex>
           </v-layout>
@@ -214,7 +222,7 @@ export default {
 
       let user = this.$store.state.user;
       this.userStories = this.stories.filter(function(obj){
-        return obj.member === user;
+        return (obj.member === user && obj.status != 'Done');
       });
     },
     
@@ -291,7 +299,7 @@ export default {
 
       let user = this.$store.state.user;
       this.userStories = this.stories.filter(function(obj){
-        return obj.member === user;
+        return (obj.member === user && obj.status != 'Done');
       });
     }
   }
